51eb7a19c106acb2492a9b74afb0923ec0f03973
ktenzweiler <ktenzweiler@gmail.com>
2023-04-21 22:52:35 -0500

    added encryption and decryption

--- app/src/main/java/com/example/aessymmetriccrypto/CryptoUtils.kt
+++ app/src/main/java/com/example/aessymmetriccrypto/CryptoUtils.kt
@@ -2,10 +2,15 @@ package com.example.aessymmetriccrypto
 
 import android.security.keystore.KeyProperties
 import android.security.keystore.KeyProtection
+import android.util.Base64
+import android.util.Log
+import java.nio.charset.StandardCharsets
 import java.security.KeyStore
 import java.util.Calendar
+import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 class CryptoUtils {
 
@@ -25,13 +30,61 @@ class CryptoUtils {
         end.add(Calendar.YEAR, 2)
 
         val entry = KeyStore.SecretKeyEntry(secretKey)
-        val protectionParameter = KeyProtection.Builder(KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
-            .setKeyValidityStart(start.time)
-            .setKeyValidityEnd(end.time)
-            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-            .build()
+        val protectionParameter =
+            KeyProtection.Builder(KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setKeyValidityStart(start.time)
+                .setKeyValidityEnd(end.time)
+                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                .build()
 
         ks.setEntry(alias, entry, protectionParameter)
+    }
+
+    fun encrypt(alias: String, plainText: String): String? {
+        val ks = KeyStore.getInstance("AndroidKeyStore").apply {
+            load(null)
+        }
+
+        val secretKey = ks.getKey(alias, null)
+
+        return try {
+            val cipher = Cipher.getInstance("AES/CBC/PKCS7PADDING")
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+
+            val cipherText = Base64.encodeToString(cipher.doFinal(plainText.toByteArray()), Base64.DEFAULT)
+            val iv = Base64.encodeToString(cipher.iv, Base64.DEFAULT)
+
+            "${cipherText}.$iv"
+        } catch (e: Exception) {
+            Log.e(TAG, "encrypt: error msg = ${e.message}")
+            null
+        }
+    }
+
+    fun decrypt(alias: String, cipherText: String): String? {
+        val ks = KeyStore.getInstance("AndroidKeyStore").apply {
+            load(null)
+        }
+
+        val secretKey = ks.getKey(alias, null)
+
+        val array = cipherText.split(".")
+        val cipherData = Base64.decode(array[0], Base64.DEFAULT)
+        val iv = Base64.decode(array[1], Base64.DEFAULT)
+
+        return try {
+            val cipher = Cipher.getInstance("AES/CBC/PKCS7PADDING")
+            val spec = IvParameterSpec(iv)
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
+
+            val clearText = cipher.doFinal(cipherData)
+
+            String(clearText, 0, clearText.size, StandardCharsets.UTF_8)
+        } catch (e: Exception) {
+            Log.e(TAG, "decrypt: error msg = ${e.message}")
+            null
+        }
     }
 }
--- app/src/main/java/com/example/aessymmetriccrypto/MainActivity.kt
+++ app/src/main/java/com/example/aessymmetriccrypto/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aessymmetriccrypto
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.aessymmetriccrypto.ui.theme.AESSymmetricCryptoTheme
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "MAIN-ACTIVITY-TAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +25,21 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                }
+
+                val alias = "secret_key"
+                val plainText = "I am some plain text that needs to be encrypted"
+
+                val cryptoUtils = CryptoUtils()
+                cryptoUtils.generateSecretKey(alias)
+
+                val cipherText = cryptoUtils.encrypt(alias, plainText = plainText)
+
+                Log.d(TAG, "onCreate: ciphertext = $cipherText")
+
+                cipherText?.let {
+                    val clearText = cryptoUtils.decrypt(alias, it)
+                    Log.d(TAG, "onCreate: cleartext decrypted = $clearText")
                 }
             }
         }
